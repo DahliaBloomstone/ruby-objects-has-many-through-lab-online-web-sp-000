718829dd6abca574b6fec6350453ef8dda16c001
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-31 20:49:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,19 @@
-#like the meals method 
+#like the meals method
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@all = [ ]
+
+def initialize(name, genre, artist)
+  @name = name
+  @genre = genre
+  @artist = artist
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+
+end 
