79eb37853f01b2db3d5ff72f6c454f6f5029d96a
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-31 21:32:26 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,16 +1,16 @@
 class Doctor
 
-  attr_reader :name
+attr_reader :name
 
-  @@all = [ ]
+@@all = [ ]
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
+def initialize(name)
+@name = name
+@@all << self
+end
 
-  def self.all
-    @@all
+def self.all
+  @@all
   end
 
   def new_appointment(patient, date)
