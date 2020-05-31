02c5b6f248830c2a64df6ac7d906cd028525ed8c
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-31 22:06:24 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,7 +4,7 @@ class Appointment
 
   @@all = []
 
-  def initialize(date ="", patient = "", doctor = "")
+  def initialize(date = "", patient = "", doctor = "")
     @patient = patient
     @doctor = doctor
     @date = date
