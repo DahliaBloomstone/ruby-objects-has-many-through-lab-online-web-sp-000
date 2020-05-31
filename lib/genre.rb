39f8d35ff7dfad841b38e90a8209307503b3c709
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-31 21:05:18 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,24 +1,22 @@
 class Genre
-
-  attr_reader :name, :genre
-  attr_accessor :song
+  attr_accessor :name
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
-  def self.all
-    @@all
+  def add_song(song)
+    @songs << song
+    song.genre = self
   end
 
   def songs
-    Song.all.select do |song|
-      song.genre == self
-    end
+    @songs
   end
 
   def artists
-    Song.all.map do |song|
+    @songs.collect do |song|
       song.artist
     end
   end
